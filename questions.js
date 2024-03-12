1dc37e0fd6f23252311fa73591b75541fb99456d
Emily Grivot <emilygrivot@hotmail.co.uk>
2024-03-12 16:01:20 +0000

    created questions.js and added manager prompt questions

---
+++ questions.js
@@ -0,0 +1,26 @@
+const questions = {
+  managerQs: [
+    {
+      type: "input",
+      name: "name",
+      message: "What is your team manager's name?"
+    },
+    {
+      type: "input",
+      name: "id",
+      message: "What is their Employee ID?"
+    },
+    {
+      type: "input",
+      name: "email",
+      message: "What is their email?"
+    },
+    {
+      type: "input",
+      name: "officeNumber",
+      message: "What is your office number?"
+    }
+  ]
+};
+
+module.exports = questions;
